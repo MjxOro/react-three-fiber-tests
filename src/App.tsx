6c50430e51907ea8389b74f5da9b7e0dfbc2d688
Matthew Oro <MatthewEOro@gmail.com>
2021-09-13 16:11:49 -0400

    fix typo on component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React, { Suspense} from 'react';
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import { Canvas } from "@react-three/fiber"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import RenderPoints from "./components/RenderCanvas/RenderPoints"
-import RenderPointsSpin from "./components/RenderCanvas/RnderPointsSpin"
+import RenderPointsSpin from "./components/RenderCanvas/RenderPointsSpin"
 
 const App: React.FC = () => {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Switch>
-        <Route path='/spin' component={RenderPointsSpin} />
-        <Route path='/' component={RenderPoints} />
-      </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/spin' component={RenderPointsSpin} />
+          <Route path='/' component={RenderPoints} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
